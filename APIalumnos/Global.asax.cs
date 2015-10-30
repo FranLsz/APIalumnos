6b9e7ee5b3bb32dd0b09ebab4cbd4f043b98dd9c
FranLsz <franlsz95develop@gmail.com>
2015-10-30 14:24:54 +0100

    first update

---
+++ APIalumnos/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace APIalumnos
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        //define que hacer cuando la app carga
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
